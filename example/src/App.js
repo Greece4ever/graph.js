fb83a2250091485108c1fd5fc8ccdf057749b06e
leonidas <programertv633@gmail.com>
2021-04-29 20:11:09 +0300

    fix margin problem

--- example/src/App.js
+++ example/src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState, createRef } from 'react'
 import {Graph2D} from 'graph2d.js'
 import InputFunction from './components/function'
 
@@ -6,11 +6,13 @@ import "./App.css"
 
 function App() {
   const [size, setSize] = useState([400, 400]);
-  const [functions, setFunctions] = useState([]);
+  const [functions, setFunctions] = useState([ [x => NaN, "red"]  ]);
+  const [selected, setSelected] = useState(null);
   
   useEffect(() => {
-    const width  = Math.min(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    const width  = Math.min(document.documentElement.clientWidth || 0, window.innerWidth || 0)  
     const height = Math.min(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+    console.log(window.innerHeight, window.outerHeight)
     setSize([(80/100) * width, height]); 
   }, [])
 
@@ -25,35 +27,70 @@ function App() {
 
   
   return (
-  <div className="App">
-    <div style={{float : 'left', width: "20%"}}>
-
-    <div className="parent" style={{
-      "width": "100%", height: window.innerHeight,
-      // border: "1px solid green",
-      background: "#181a1b",
-      // borderRight: "1px solid #1e1f1f"
-      border: "1px solid rgba(33, 32, 31, 0.1)"
-      }}>
-
-      <div style={{
-        "width": "100%",
-        "height": "48px",
-        "background" : "#202224",
+  <div className="App" style={{
+    width: "100%",
+    height: "100%",
+    overflow: "hidden !important"
+  }} >
+    <div style={{float : 'left', width: "20%", height: "100vh"}}>
+      <div className="parent" style={{
+        // "overflow": "auto",
+        overflowX: "hidden",
+        width: "100%", 
+        height: "100%",
+        background: "#181a1b",
+        padding: "0 !important",
+        margin:  "0 !important",
         border: "1px solid rgba(33, 32, 31, 0.1)",
-        // "borderRight": "none"
-      }}></div>
+        display: "grid",
+        gridTemplateRows: "5%"
+        }}>
+
+        <div style={{
+          position: "relative",
+          "width": "100%",
+          "height": "48px",
+          "background" : "#202224",
+          border: "1px solid rgba(33, 32, 31, 0.1)",
+          // "borderRight": "none"
+        }}>
+
+          <button onClick={() => {
+            setFunctions(prev => [...prev, [ x => NaN, "red" ] ])
+          }} 
+          className="btn" style={{"position" : "absolute", left: 0, color: "#40648d"}}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+            </svg>
+          </button>
 
 
+          <button style={{"position": "absolute", "right": 0, color: "#f15e53"}} className="btn">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+              <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+            </svg>
+          </button>
 
-      <InputFunction />
+        </div>
+
+        <div style={{width: "100%", height: "100%", overflow: "auto"}}
+        >
+          {functions.map( 
+            (func, index) => <InputFunction functions={functions} setFunctions={setFunctions} index={index} />
+          )}
+        </div>
+
         
-    </div>    
-
+      </div>    
     </div>
 
+
+
     <div style={{float: 'right', width: "80%"}}>
-      <Graph2D functions={functions} width={size[0]} height={size[1]} />
+      <Graph2D canvasStyle={{
+        
+          
+          } } functions={functions} width={size[0]} height={size[1]} />
     </div>
 
     </div>
--- example/src/components/function.js
+++ example/src/components/function.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState, useRef} from 'react'
 import { addStyles, EditableMathField } from 'react-mathquill'
 import MathLive from 'mathlive';
+import {evaluate} from 'mathjs';
 
 
 const color_blue = "#2c5a84";
@@ -14,7 +15,10 @@ const randomColor = () => {
 
 const allowed_keys = Array.from("0123456789()x+-/*()")
 
-const InputFunction = () => {
+let scope = {'ln': Math.log}
+
+
+const InputFunction = (props) => {
     const [math_expr, setMathExpr] = useState("");
     const [color, setColor] = useState(color_grey);
     const [functionColor, setFunctionColor] = useState(randomColor());
@@ -23,21 +27,21 @@ const InputFunction = () => {
     const math_input = useRef();
 
     useEffect(() => {
-        // if (!math_expr)
-        //     return;
-        // console.log("second", field.getValue('ascii-math'))
-    }, [math_expr])
-
-    useEffect(() => {
         if (!math_input.current)
             return;
 
        let q=  MathLive.makeMathField(math_input.current,  {
             virtualKeyboardMode: "manual",
             virtualKeyboardLayout: 'dvorak',
+            onFocus: () => {
+                setColor(color_blue);
+            },
             onContentDidChange: (mf) => {
-                setMathExpr( mf.getValue('ascii-math')  )
-                console.log( mf.getValue('ascii-math')  )
+                setMathExpr( mf.getValue('ascii-math')
+                .replaceAll("⋅", "*")
+            
+            )
+                
             }
         });
 
@@ -46,10 +50,32 @@ const InputFunction = () => {
     }, [math_input])
 
 
+
+    useEffect(() => {
+        try {
+            console.log("func -> " , math_expr  )
+            let func = evaluate( math_expr, scope  );
+            if (typeof func != "function")
+                return;
+            try { 
+                console.log("--> ", func(3.1415))
+                props.setFunctions(prev => {
+                    let prev_ = prev.slice();
+                    console.log("set");
+                    prev_[props.index] = [func, functionColor];
+                    return prev_;
+                })
+
+            } catch(err2) {console.log(err2.message)}
+        } catch(err) {console.log(err.message)}
+    }, [math_expr])
+
+
     return (
         <div 
 
         onFocus={() => {
+            field.focus();
             setColor(color_blue);
         }}
 
@@ -58,15 +84,18 @@ const InputFunction = () => {
         }}
         
         onClick={() => {
+            field.focus();
             setColor(color_blue);
-        }} style={{
+        }}
+        
+        style={{
             width: "100%",
             height: "58px",
             border: `1px solid ${color}`,
             justifyContent: "space-around",
             display: "flex",
             justifyContent: "center",
-            alignItems: "center"
+            alignItems: "center",
         }}>
             
 
@@ -79,19 +108,23 @@ const InputFunction = () => {
                 border: `1px solid ${color == color_grey ? "#252727" : color_blue}`
             }}> 
 
-            <div style={{
-                "position" : "absolute",
-                "left" : "50%",
-                transform: "translate(-50%, -50%)",
-                "top" : "50%",
+            <label className="blured" style={{"position": "absolute", fontSize: "10px", left: 0, top: 0}}>{props.index + 1}</label>
+            
+                <div style={{
+                    "position" : "absolute",
+                    "left" : "50%",
+                    transform: "translate(-50%, -50%)",
+                    "top" : "50%",
 
-             }}>
-                <svg style={{
-                    "color" : functionColor
-                }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-lightning-fill" viewBox="0 0 16 16">
-                    <path d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641l2.5-8.5z"/>
-                </svg>
-            </div>
+                }}>
+
+                    <svg style={{
+                        "color" : functionColor,
+                        
+                    }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-lightning-fill" viewBox="0 0 16 16">
+                        <path d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641l2.5-8.5z"/>
+                    </svg>
+                </div>
 
 
             </div>
@@ -102,22 +135,15 @@ const InputFunction = () => {
                 float: 'left',
                 width: "calc( 100% - 38px )",
             }}>
-            
-            
-            <div ref={math_input} //id="peos"
-                // onInput={(e) => {
-                //     console.log(e.target.getValue('ascii-math'))
-                // }}
 
-
-                style={{
-                    position: "relative",
-                    marginLeft: "10px",
-                    fontSize: "120%",
-                    width: "calc(100% - 10px)"
-                }}
-            
-            ></div>            
+                <div ref={math_input} 
+                    style={{
+                        position: "relative",
+                        marginLeft: "10px",
+                        fontSize: "120%",
+                        width: "calc(100% - 10px)"
+                    }}
+                ></div>            
 
             </div>
         
